77d31ba4ceac7b8a48318521818ef6702cca6f8e
Agustin-morcillo <morcilloagustin95@gmail.com>
2021-07-21 15:26:02 -0300

    search-bar

--- components/ui/SearchBar.js
+++ components/ui/SearchBar.js
@@ -1,8 +1,9 @@
-import React from "react"
+import React, { useState } from "react"
 
 import { SearchInput, SubmitButton } from "/styles/theme/ui/SearchBar-theme"
 
 export default function SearchBar() {
+  const [userSearch, setUserSearch] = useState("")
   return (
     <form className="search-bar-form">
       <SearchInput type="text" placeholder="Buscar Productos" />
